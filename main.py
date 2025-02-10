d7beff56958f46cacb67e294af85d4a3b0bfd034
RanaKumar <38910803+RanaTieto@users.noreply.github.com>
2025-02-10 20:00:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ async def get_index_html() -> FileResponse:
 
 def main() -> None:
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)  # noqa: S104
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # noqa: S104
 
 
 if __name__ == "__main__":
